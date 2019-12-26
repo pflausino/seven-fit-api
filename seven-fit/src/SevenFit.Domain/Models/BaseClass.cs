2f66a867b6203e081a958f2159ed996d8fc1da45
Paulo Flausino <paulo.flausino@iatec.com>
2019-12-26 17:23:47 -0300

    first load

---
+++ seven-fit/src/SevenFit.Domain/Models/BaseClass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SevenFit.Domain.Models
+{
+    public abstract class BaseClass
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ seven-fit/src/SevenFit.Domain/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SevenFit.Domain.Models
+{
+    public abstract class Person : BaseClass
+    {
+        public string Email { get; private set; }
+
+        public Person(string email)
+        {
+            SetEmail(email);
+        }
+
+        private void SetEmail(string email)
+        {
+            if (email.Contains("@"))
+                Email = email;
+            else
+                throw new ArgumentException(nameof(email));
+        }
+    }
+}
